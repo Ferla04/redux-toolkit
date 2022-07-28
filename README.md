e32f5c48090938ddcaf89d53e2489a514c18643c
Ferla04 <fer.velas135@gmail.com>
2022-07-28 07:44:33 -0500

    usar axios y thunks

---
+++ README.md
@@ -0,0 +1,5 @@
+# Redux
+
+## Thunks
+Es una funcion que va a ejecutar una tarea asincrona y al terminar va
+a disparar una acción a algunos de nuestros reducers
--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,14 @@ function App() {
   const dispatch = useDispatch()
 
   return (
-    <div className="App">
-      <h1>count is { value }</h1>
-      <div className="card">
-        <button onClick={ () => dispatch( increment() ) }> Increment </button>
-        <button onClick={ () => dispatch( decrement() ) }> Decrement </button>
-        <button onClick={ () => dispatch( incrementBy(2) ) }> Increment by 2 </button>
+    <div className='center-app'>
+      <div className="App">
+        <h1>count is { value }</h1>
+        <div className="card">
+          <button onClick={ () => dispatch( increment() ) }> Increment </button>
+          <button onClick={ () => dispatch( decrement() ) }> Decrement </button>
+          <button onClick={ () => dispatch( incrementBy(2) ) }> Increment by 2 </button>
+        </div>
       </div>
     </div>
   )
